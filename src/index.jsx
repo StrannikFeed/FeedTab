15ecc95bb06233f3c1dd81d129f524c19c8197b0
StrannikFeed <artem-belyaev-2004@bk.ru>
2026-06-16 00:25:06 +0400

    readme в прошлое состояние

--- src/index.jsx
+++ src/index.jsx
@@ -5,8 +5,5 @@ import App from './App';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <App />
-    <App />
-    <App />
   </React.StrictMode>
 );
